8b760353256d7cb128cb28e8daed26edf5fa9a11
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-03-15 17:43:11 +0800

    logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,10 @@
     <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
             <li>
-                <a href="" class="p-3">Home</a>
+                <a href="/" class="p-3">Home</a>
             </li>
             <li>
-                <a href="" class="p-3">Dashboard</a>
+                <a href={{ route('dashboard') }} class="p-3">Dashboard</a>
             </li>
             <li>
                 <a href="" class="p-3">Post</a>
@@ -26,11 +26,14 @@
         <ul class="flex items-center">
             @auth
                 <li>
-                    <a href="" class="p-3">Muhammed</a>
+                    <a href="" class="p-3">{{ auth()->user()->name }}</a>
                 </li>
 
                 <li>
-                    <a href="" class="p-3">Logout</a>
+                    <form action="{{ route('logout') }}" method="POST" class="inline p-3">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
                 </li>
             @endauth
             @guest
