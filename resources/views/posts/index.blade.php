84379a6f881df43879404bb92a33ccb2ce522310
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-03-31 11:17:49 +0100

    delete a post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -28,7 +28,17 @@
                         <a href="font-bold">{{ $post->user->name }}</a>
                         <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
                         <p class="mb-2">{{ $post->body }}</p>
+                        
+                        <div>
+                          <form action="{{ route('posts.destroy', $post) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                           <button type="submit" class="text-blue-500">Delete</button>
+                           </form> 
+                        </div>
+
                         <div class="flex items-center">
+                            @auth
                           @if (!$post->likedBy(auth()->user()))
                             <form action="{{ route('posts.like', $post) }}" class="mr-1" method="POST">
                                 @csrf
@@ -41,8 +51,11 @@
                                 <button type="submit" class="text-blue-500">Unlike</button>
                             </form>
                             @endif
+                           
+                            @endauth
                             <span>{{ $post->likes->count() }} {{ Str::plural('like',$post->likes->count())}}</span>
                         </div>
+
                     </div>
                 @endforeach
                 {{ $posts->links() }}
