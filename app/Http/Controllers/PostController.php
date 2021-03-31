ce8bfc3286215f094f38dabd0b955caee8af1f5f
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-03-31 11:51:29 +0100

    delete post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::paginate(2);
+        $posts = Post::latest()->paginate(2);
 
         return view('posts.index', [
             'posts' => $posts
@@ -28,5 +28,12 @@ class PostController extends Controller
 
      return back();
 
+    }
+
+    public function destroy(Post $post)
+    {
+       $post -> delete();
+
+       return back();
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -28,7 +28,8 @@
                         <a href="font-bold">{{ $post->user->name }}</a>
                         <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
                         <p class="mb-2">{{ $post->body }}</p>
-                        
+
+                        @if ($post->ownedBy(auth()->user())) 
                         <div>
                           <form action="{{ route('posts.destroy', $post) }}" method="post">
                             @csrf
@@ -36,6 +37,7 @@
                            <button type="submit" class="text-blue-500">Delete</button>
                            </form> 
                         </div>
+                        @endif
 
                         <div class="flex items-center">
                             @auth
